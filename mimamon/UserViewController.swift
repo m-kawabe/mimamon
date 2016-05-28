b9b4b872f54fa396490326074540fb3dbda33513
Kawabe <b1012067@fun.ac.jp>
2016-05-28 15:11:24 +0900

    add Top,User,DetailViewController

---
+++ mimamon/UserViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  mimamon
+//
+//  Created by 河辺雅史 on 2016/05/28.
+//  Copyright © 2016年 fun. All rights reserved.
+//
+
+import UIKit
+
+class UserViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func BackButtonAction(sender: AnyObject) {
+        self.navigationController?.popViewControllerAnimated(true);
+    }
+    
+}
+
